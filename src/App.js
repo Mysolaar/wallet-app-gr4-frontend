5a12a277dd071a71b420ac3bbfa2a29161454ae5
Wioleta-Kapinos <kapinos.wioleta19@gmail.com>
2023-07-18 02:20:52 +0200

    create modal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from "./components/Header/Header";
+import ModalLogout from "./components/ModalLogout/ModalLogout";
 
 function App() {
   return (
   <div>
+     <ModalLogout />
     <Header />
     Lets get it started!!!
     </div>
