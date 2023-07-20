035a1e738dfd676d7c7d2ee0f02232c1004519c0
SebastianSzczypiorowski <110854817+SebastianSzczypiorowski@users.noreply.github.com>
2023-07-20 15:54:07 +0200

    1

---
+++ src/pages/login/loginPage.jsx
@@ -0,0 +1,12 @@
+import { LoginLayout } from "./loginLayout";
+import LoginForm from "../../components/LoginForm/loginForm";
+
+export default function LoginPage() {
+  return (
+    <div>
+      <LoginLayout>
+        <LoginForm />
+      </LoginLayout>
+    </div>
+  );
+}
---
+++ src/pages/register/registerPage.jsx
@@ -0,0 +1,12 @@
+import { RegisterLayout } from "./RegisterLayout";
+import RegisterForm from "../../components/RegisterForm/RegisterForm";
+
+export default function RegisterPage() {
+  return (
+    <div>
+      <RegisterLayout>
+        <RegisterForm />
+      </RegisterLayout>
+    </div>
+  );
+}
