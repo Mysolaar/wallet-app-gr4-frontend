0d998dc7ed8cbdb6af653f679e351374a61567f0
TomekGrzybek <t.grzybowski94@gmail.com>
2023-07-23 23:12:16 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import Header from "./components/Header/Header";
 // import ModalLogout from "./components/ModalLogout/ModalLogout";
 // import Loader from "./components/Loader/Loader";
 // import RegisterPage from "./pages/Register/RegisterPage";
-import ModalAddTransactions from "./components/ModalAddTransactions/ModalAddTransactions";
+// import ModalAddTransactions from "./components/ModalAddTransactions/ModalAddTransactions";
+import Table from "./components/Table/Table.jsx";
 // import LoginPage from "./pages/Login/LoginPage";
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <ModalLogout />
       <Loader /> */}
       <Header />
-      <ModalAddTransactions type="add" />
+      {/* <ModalAddTransactions type="add" /> */}
+      <Table />
     </div>
   );
 }
---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,47 @@
+import styles from "./Table.module.css";
+
+const data = [
+  { category: "Main expenses", sum: 8700 },
+  { category: "Products", sum: 3800 },
+  { category: "Car", sum: 1500 },
+];
+
+const Table = () => {
+  const totalSum = data.reduce((acc, item) => acc + item.sum, 0);
+  //TODO dodać fetch danych
+  return (
+    <div className={styles["table-container"]}>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th className="table-header">Category</th>
+            <th className="table-header">Sum</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index} className={styles["data-row"]}>
+              <td>{item.category}</td>
+              <td>{formatNumber(item.sum)}</td>
+            </tr>
+          ))}
+          <tr>
+            <td>Expenses:</td>
+            <td className={styles.expenses}>{formatNumber(totalSum)}</td>
+          </tr>
+          <tr>
+            <td>Income:</td>
+            <td className={styles.income}>{formatNumber(100000)}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const formatNumber = (number) => {
+  return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$& ");
+  // .replace(".", ",");
+};
+
+export default Table;
