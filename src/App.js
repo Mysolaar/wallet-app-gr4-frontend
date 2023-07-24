e22ff30cde9569d64e03778725927f05771a2d29
SebastianSzczypiorowski <110854817+SebastianSzczypiorowski@users.noreply.github.com>
2023-07-24 12:03:27 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,99 @@
-// import Header from "./components/Header/Header";
-// import ModalLogout from "./components/ModalLogout/ModalLogout";
+// import { useEffect, lazy } from "react";
+// import { useDispatch } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import { PrivateRoute } from "./HOC/PrivateRoute";
+// import { PublicRoute } from "./HOC/PublicRoute";
+
+// import { fetchCurrentUser } from "./redux/auth/authOperations";
+
 // import Loader from "./components/Loader/Loader";
-// import RegisterPage from "./pages/Register/RegisterPage";
-import Store from "./redux/store";
+import RegisterPage from "./pages/Register/RegisterPage";
 import LoginPage from "./pages/Login/LoginPage";
+
+// import { useAuth } from "./hooks/useAuth";
+
+//LAZY LOADING:
+
+// const RegisterPage = lazy(() => import("./pages/Register/RegisterPage"));
+// const LoginPage = lazy(() => import("./pages/Login/LoginPage"));
+// const DashboardPage = lazy(() => import("./pages/DashboardDashboardPage"));
+// const StatisticsPage = lazy(() => import("./pages/Statistics/StatisticsPage"));
+// const CurrencyPage = lazy(() => import("./pages/Currency/CurrencyPage"));
+
 function App() {
   return (
-    <div>
-      <LoginPage />
-      {/* <ModalLogout />
-      <Loader />
-      <Header /> */}
-    </div>
+    <Routes>
+      <Route index element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
+
+      <Route path="*" element={<LoginPage />} />
+    </Routes>
   );
 }
 
 export default App;
+
+// DO WYKORZYSTANIA PÓŹNIEJ w App
+
+// const dispatch = useDispatch();
+//   const { isRefreshing } = useAuth();
+
+//   useEffect(() => {
+//     dispatch(fetchCurrentUser());
+//   }, [dispatch]);
+
+//   return isRefreshing ? (
+//     <Loader />
+//   ) : (
+//     <Routes>
+//       <Route path="/" element={<RegisterPage />} />
+//       <Route path="/login" element={<LoginPage />} />
+
+//       <Route path="*" element={<LoginPage />} />
+//     </Routes>
+//   );
+// }
+
+//----------------------------
+
+// return isRefreshing ? (
+//     <Loader />
+//   ) : (
+//     <Routes>
+//       <Route
+//         path="/"
+//         element={
+//           <PublicRoute redirectTo="/home" component={<RegisterPage />} />
+//         }
+//       />
+//       <Route
+//         path="/login"
+//         element={<PublicRoute redirectTo="/home" component={<LoginPage />} />}
+//       />
+
+//       <Route
+//         path="/home"
+//         element={
+//           <PrivateRoute redirectTo="/login" component={<DashboardPage />} />
+//         }
+//       />
+//       <Route
+//         path="/statistics"
+//         element={
+//           <PrivateRoute redirectTo="/login" component={<StatisticsPage />} />
+//         }
+//       />
+//       <Route
+//         path="/currency"
+//         element={
+//           <PrivateRoute redirectTo="/login" component={<CurrencyPage />} />
+//         }
+//       />
+
+//       <Route path="*" element={<LoginPage />} />
+//     </Routes>
+//   );
+// }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import store from "./redux/store";
+import store, { persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import "modern-normalize/modern-normalize.css";
 import "./index.css";
@@ -10,9 +12,13 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/wallet-app-gr4-frontend">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 );
