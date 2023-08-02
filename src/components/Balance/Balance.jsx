234982b9f0e3c16b002914467bce9ab9479f26dd
Magdalena Grądek <magdalena.gradek@gmail.com>
2023-08-02 18:05:04 +0200

    balance final look

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -6,25 +6,23 @@ import { useAuth } from "../../hooks/useAuth";
 export default function Balance() {
   const { isAuth } = useAuth();
   const balance = useSelector(selectBalance);
-  // const formatBalance = balance.toFixed(2);
-  // const displayBalance = formatBalance.toLocaleString("pl-PL");
 
-  const displayBalance = balance.toLocaleString("en-EN", {
+  const formatedBalance = balance.toLocaleString("pl-PL", {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
-    useGrouping: true, // Włącz grupowanie dla separatora tysięcy
+    useGrouping: true,
+    style: "currency",
+    currency: "PLN",
   });
 
-  console.log(balance);
+  const displayBalance = formatedBalance.replace(",", ".");
 
   return (
     <div className={styles.balance}>
       <div className={styles.balance__text}>Your balance</div>
       <div className={styles.balance__amount}>
-        <span>zł</span> {isAuth ? displayBalance : ""}
+        {isAuth ? displayBalance : ""}
       </div>
-      {/* <div className={styles.balance__amount}>PLN {balance}
-      </div> */}
     </div>
   );
 }
