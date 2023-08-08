e0be4d781855f948f89d9460a80870ff5f5c96de
SebastianSzczypiorowski <110854817+SebastianSzczypiorowski@users.noreply.github.com>
2023-08-08 20:50:29 +0200

    1

--- src/pages/Login/LoginLayout.js
+++ src/pages/Login/LoginLayout.js
@@ -2,11 +2,13 @@ import css from "./LoginPage.module.css";
 
 export const LoginLayout = (props) => {
   return (
-    <div className={css.a}>
-      <div className={css.b}>
-        <p>Finance App</p>
+    <div className={css.d}>
+      <div className={css.a}>
+        <div className={css.b}>
+          <p>Finance App</p>
+        </div>
+        <div className={css.c}>{props.children}</div>
       </div>
-      <div className={css.c}>{props.children}</div>
     </div>
   );
 };
