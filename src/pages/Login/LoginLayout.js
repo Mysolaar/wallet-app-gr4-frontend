f756ef03a3e34ea01f5812d375a3d671627c421e
SebastianSzczypiorowski <110854817+SebastianSzczypiorowski@users.noreply.github.com>
2023-09-04 22:01:34 +0200

    1

--- src/pages/Login/LoginLayout.js
+++ src/pages/Login/LoginLayout.js
@@ -2,9 +2,11 @@ import css from "./LoginPage.module.css";
 
 export const LoginLayout = (props) => {
   return (
-    <div className={css.background}>
+    <div className={css.container}>
       <div className={css.layout}>
-        <div className={css.logo}></div>
+        <div className={css.logo_container}>
+          <div className={css.logo}></div>
+        </div>
         <div className={css.form}>{props.children}</div>
       </div>
     </div>
