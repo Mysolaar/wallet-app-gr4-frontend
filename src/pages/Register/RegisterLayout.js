a47090a842865ddb7da3190f6b6da7b9336a90b5
SebastianSzczypiorowski <110854817+SebastianSzczypiorowski@users.noreply.github.com>
2023-09-06 19:15:27 +0200

    1

--- src/pages/Register/RegisterLayout.js
+++ src/pages/Register/RegisterLayout.js
@@ -2,9 +2,11 @@ import css from "./RegisterPage.module.css";
 
 export const RegisterLayout = (props) => {
   return (
-    <div className={css.background}>
+    <div className={css.container}>
       <div className={css.layout}>
-        <div className={css.logo}></div>
+        <div className={css.logo_container}>
+          <div className={css.logo}></div>
+        </div>
         <div className={css.form}>{props.children}</div>
       </div>
     </div>
